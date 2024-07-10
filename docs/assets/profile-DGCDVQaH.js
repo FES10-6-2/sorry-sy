936c873081b996a87e6e870ac4e1a8059ed98bd3
songyi225 <sy920225@naver.com>
2024-07-10 17:04:41 +0900

    build test

---
+++ docs/assets/profile-DGCDVQaH.js
@@ -0,0 +1,1 @@
+const e=document.querySelector(".profile__button__edit"),o=document.querySelector(".profile__button__done");function t(){location.href="/pages/profilesForEdit.html"}function i(){location.href="/pages/profiles"}e&&e.addEventListener("click",t);o&&o.addEventListener("click",i);
